60617932fcebd8cd971555621942d95097c254ce
Evgeniiy-85 <evgeniy.aynulov@yandex.ru>
2025-01-20 16:35:13 +0400

    Правки

--- views/products/product.php
+++ views/products/product.php
@@ -3,27 +3,36 @@ use yii\helpers\Html;
 use app\components\Helpers;
 use app\models\Products;
 
-$this->title = isset($category) ? $category['cat_title'] : $product['prod_title'];
+$this->title = $product['prod_title'];
 $this->params['breadcrumbs'] = Products::getBreadCrumbs($category, $product);?>
 
-<div class="product-card">
-    <div class="product_cover">
-        <img src="/load/products/<?=$product['prod_image'];?>">
-    </div>
+<div class="site-catalog">
+    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="product-card_center">
-        <a class="product-card_title" href="/products/<?=$product['prod_alias'];?>"><?=$product['prod_title'];?></a>
+    <div class="product">
+        <div class="product-card">
+            <div class="product_cover">
+                <img src="/load/products/<?=$product['prod_image'];?>">
+            </div>
 
-        <div class="card_bottom">Наличите: в наличии</div>
-    </div>
+            <div class="product-card_center">
+                <a class="product-card_title" href="/products/<?=$product['prod_alias'];?>"><?=$product['prod_title'];?></a>
 
-    <div class="product-card_right">
-        <div class="product-price">
-            <?=Helpers::formatPrice($product['prod_price']);?> ₽
-        </div>
+                <div class="product-by product-by-one_line">
+                    <div class="product-price">
+                        <?=Helpers::formatPrice($product['prod_price']);?> ₽
+                    </div>
 
-        <div class="product-by">
-            <a class="button button-ui" href="/buy/<?=$product['prod_alias'];?>">Купить</a>
+                    <a class="button button-ui" href="/buy/<?=$product['prod_alias'];?>">Купить</a>
+                </div>
+                <div class="card_bottom">Наличите: в наличии</div>
+            </div>
+
+            <div class="product-card_right">
+
+
+
+            </div>
         </div>
     </div>
 </div>
