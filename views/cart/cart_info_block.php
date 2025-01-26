1811bdb098e8adb85e5096cc3d60fa9ba1e0237d
Evgeniiy-85 <evgeniy.aynulov@yandex.ru>
2025-01-26 17:56:03 +0400

    Правки по оформлению заказов

---
+++ views/cart/cart_info_block.php
@@ -0,0 +1,39 @@
+<?php
+use yii\helpers\Html;
+use app\components\Helpers;
+use yii\helpers\Url;
+use app\models\Product;?>
+
+<div class="order-products_list">
+    <div class="order-header">
+        <div class="cart-title">Список товаров</div>
+    </div>
+
+    <div class="order-body">
+        <?$first_key = array_key_first($cart->products);
+        foreach($cart->products as $prod_id => $product):
+            $quantity = $cart->quantity[$prod_id];;?>
+            <?if($prod_id !== $first_key):?>
+                <hr>
+            <?endif;?>
+
+            <div class="order-product">
+                <div class="order-product_cover">
+                    <img src="/load/products/<?=$product->prod_image;?>">
+                </div>
+
+                <div class="order-product_info">
+                    <div class="order-product_title"><?=Html::encode("{$product->prod_title} ({$quantity}шт.)");?></div>
+                    <div class="order-product_price"><nobr><?=Helpers::formatPrice($product->prod_price * $quantity);?> руб.</nobr></div>
+                </div>
+            </div>
+        <?endforeach;?>
+    </div>
+
+     <div class="cart-footer">
+        <div class="cart-footer_left">
+            <strong>Итого: </strong>
+            <span><?=Helpers::formatPrice($cart->total);?></span>
+        </div>
+     </div>
+</div>
--- views/cart/checkout.php
+++ views/cart/checkout.php
@@ -8,27 +8,37 @@ $this->title = 'Оформление заказа';?>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="row">
-        <div class="col-md-5">
+        <div class="col-md-9">
+            <?=$this->render('cart_info_block', [
+                'order' => $order,
+                'cart' => $cart,
+            ]);?>
+        </div>
+
+        <div class="col-md-9" style="margin-top: 40px;">
             <div class="card checkout-form_wrap">
-                <?$form = ActiveForm::begin([
-                    'id' => 'form-checkout',
-                    'options' => [
-                        'enctype' => 'multipart/form-data'
-                    ]
-                ]);?>
+                <div class="row">
+                    <div class="col-md-7">
+                        <?$form = ActiveForm::begin([
+                            'id' => 'form-checkout',
+                            'options' => [
+                                'enctype' => 'multipart/form-data'
+                            ]
+                        ]);?>
 
-                <div class="card-body">
-                    <?= $form->field($order, 'client_email')->input('email', ['autofocus' => true]); ?>
-                    <?= $form->field($order, 'client_name')->input('text'); ?>
-                    <?= $form->field($order, 'client_surname')->input('text'); ?>
-                    <?= $form->field($order, 'client_phone')->input('text'); ?>
+                        <div class="card-body">
+                            <?= $form->field($order, 'client_email')->input('email', ['autofocus' => true]); ?>
+                            <?= $form->field($order, 'client_name')->input('text'); ?>
+                            <?= $form->field($order, 'client_surname')->input('text'); ?>
+                            <?= $form->field($order, 'client_phone')->input('text'); ?>
+
+                            <div class="mt-4">
+                                <?= Html::submitButton('Подвердить заказ', ['class' => 'button button-ui btn_a-primary button-small', 'name' => 'next']) ?>
+                            </div>
+                        </div>
+                        <?ActiveForm::end();?>
+                    </div>
                 </div>
-
-                <div class="card-footer text-right">
-                    <?= Html::submitButton('Подвердить заказ', ['class' => 'button button-ui btn_a-primary button-small', 'name' => 'next']) ?>
-                </div>
-
-                <?ActiveForm::end();?>
             </div>
         </div>
     </div>
